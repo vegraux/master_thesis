dba86afb77bf40d4d6bd299244028c520ee050c4
vegard ulriksen solberg <vegard.ulriksen.solberg@nmbu.no>
2019-02-18 10:03:22 +0100

    separated simple and sparse env classes

--- gym_power/envs/__init__.py
+++ gym_power/envs/__init__.py
@@ -3,7 +3,8 @@
 """
 
 """
-from gym_power.envs.power_env import PowerEnv, PowerEnvSparse
+from gym_power.envs.power_env import PowerEnv
+from gym_power.envs.power_env_sparse import PowerEnvSparse
 
 __author__ = 'Vegard Solberg'
 __email__ = 'vegard.ulriksen.solberg@nmbu.no'
